d8e2029a4121b75f6537662fa28537bce8d505ee
islam solaiman <islam.solaiman92@gmail.com>
2023-10-28 09:41:20 -0700

    RESTful API

---
+++ api/v1/views/index.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+    Contains the index view for the API.
+'''
+from flask import jsonify
+from models import storage
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Returns JSON """
+    return jsonify(status="OK")
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stat():
+    """returns the number of each objects by type"""
+    return jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        places=storage.count('Place'),
+        reviews=storage.count('Review'),
+        states=storage.count('State'),
+        users=storage.count('User')
+    )
